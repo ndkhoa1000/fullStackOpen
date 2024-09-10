3c3bd9a8d64fa6142756ad294f2af6232844d2b6
L8duck <ndkhoa1000@gmail.com>
2024-09-10 18:47:04 +0700

    add ex2.18, 2.19, 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,45 +1,74 @@
 import { useState,useEffect } from "react"
 import axios from "axios"
 
-const Display = ({countries,dataReady}) => {
-  const [Data, setData] = useState(null)
-  // console.log('countries', countries)
-  // console.log('Data', Data)
-  useEffect(() => {
-    if(countries && countries.length === 1){
-      axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${countries[0]}`)
-      .then(res => setData(res.data))}
-    else setData(null)
-  },[countries])
+const Display = ({countries,search}) => {
+  const [countryFilter, setCountryFilter] = useState(null)
+  const [countryData, setCountryData] = useState(null)
+  const [weather,setWeather] = useState(null)
 
-  if(!dataReady)
+  useEffect(() => {  // setFilter base on search
+    if(countries) 
+      setCountryFilter(countries.filter(country => country.toLowerCase().includes(search)))
+    else setCountryFilter(null)
+  },[search])
+
+  useEffect(() => { //if only one country then fetch data
+    if(countryFilter && countryFilter.length === 1){
+      axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${countryFilter[0]}`)
+      .then(res => {
+        setCountryData(res.data)
+        // console.log('countryData', countryData)
+      })
+    }
+    else setCountryData(null)
+  },[countryFilter])
+
+  useEffect(() => {
+      if(countryData){
+        const lat = countryData.latlng[0]
+        const lng = countryData.latlng[1]
+        const apiKey = import.meta.env.VITE_SOME_KEY
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apiKey}&units=metric`)
+        .then(res => {
+          setWeather(res.data)
+          // console.log('weather',weather)
+        })
+      }
+      else setWeather(null)
+  },[countryData])
+
+  if(!countries)
     return (<p> waiting for data ...</p>)
-  else
-    return countries && countries.length ===1 ? (
-            !Data ? (<p>waiting for data ...</p>)
+  if(countries && countryFilter)
+    return countryFilter.length ===1 ? (
+            !(countryData && weather) ? (<p>waiting for data ...</p>)
             :
             (
               <>
-                <h3>{Data.name.common}</h3>
-                <p>capital {Data.capital}</p>
-                <p>area {Data.area} km</p>
+                <h3>{countryData.name.common}</h3>
+                <p>capital {countryData.capital}</p>
+                <p>area {countryData.area} km</p>
                 <strong>languages</strong>
                 <ul>
-                  {Object.values(Data.languages).map(language => (
+                  {Object.values(countryData.languages).map(language => (
                     <li key={language}>{language}</li>
                   ))}
                 </ul>
-                <img src={Data.flags.png} alt={Data.name.common} sizes="100" />
+                <img src={countryData.flags.png} alt={countryData.name.common} sizes="100" />
+                <h3>Weather in {countryData.capital}</h3>
+                <p>temperature {weather.main.temp} Celcius</p>
+                <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" />
+                <p>wind {weather.wind.speed} m/s</p>
               </>
             )
         )
-      : countries && countries.length >10 ? (
+      : countryFilter.length >10 ? (
         <p>Too many matches, specify another filter</p>
       )
-      : countries && countries.length >1 ? (
+      : countryFilter.length >1 ? (
         <ul>
-          {countries.map(country => 
-            <li key={country}>{country} <button>show</button></li>
+          {countryFilter.map(country => 
+            <li key={country}>{country} <button onClick={() => setCountryFilter([country])}>show</button></li>
           )}
         </ul>      
       )
@@ -48,7 +77,7 @@ const Display = ({countries,dataReady}) => {
       )  
 }
 function App() {
-  const [filter, setFilter] = useState(null)
+  const [search, setSearch] = useState(null)
   const [countries, setCountries] = useState(null)
   
   useEffect(() => {
@@ -61,13 +90,12 @@ function App() {
   }, [])
   const handleSearch = (event) => {
     event.preventDefault()
-    const search = event.target.value
-    setFilter(countries.filter(country => country.toLowerCase().includes(search)))
+    setSearch(event.target.value)
   }
   return (
     <>
       <p>find countries <input onChange={handleSearch} type="text" /></p>
-      <Display countries={filter} dataReady={countries}/>
+      <Display search={search} countries={countries}/>
     </>
   )
 }
